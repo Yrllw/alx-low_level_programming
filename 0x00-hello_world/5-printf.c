198fd9d28dae3b725299fc9fafd018bc63e1b4d4
Yrllw <olaleyewahab@gmail.com>
2022-03-14 06:32:58 -0700

    245

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints the phrase with proper grammer,
- * but teh outcome is a piece of art,
- * Return: 0 if exited properly,non-zero otehrwise
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n);
-	return (0);
-}
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0);
+} 
